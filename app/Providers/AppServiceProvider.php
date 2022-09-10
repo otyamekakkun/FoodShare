6fa53a8f7326cc933671c11c64a363d7bbe7c930
kakutoikemen <karasisuki4902@gmail.com>
2022-09-10 15:27:13 +0900

    Logのデバッグシステム完了　15:25

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //商用環境以外だった場合は,sqlログを出力させる。提出する際はコメントアウトしとく。
+        if (config('app.env') !== 'production') {
+            \DB::listen(function ($query) {
+                \Log::info("Query Time:{$query->time}s] $query->sql");
+            });
     }
+}
 }
