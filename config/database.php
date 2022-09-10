457e8d6848a9bceb9a00c752e4e087d23c27b718
kakutoikemen <karasisuki4902@gmail.com>
2022-09-10 16:04:22 +0900

    データベースの名前と　フォルダーをhaikishareに変更

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'kindevery'),
+            'database' => env('DB_DATABASE', 'haikishare'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'root'),
             'unix_socket' => env('DB_SOCKET', '/Applications/MAMP/tmp/mysql/mysql.sock'),
