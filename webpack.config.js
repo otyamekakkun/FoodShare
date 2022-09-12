bd12e3b0b260b4079e34dac66017aae24488c05f
kakutoikemen <karasisuki4902@gmail.com>
2022-09-12 10:43:00 +0900

    とりあえず仕様の説明をwebpack.config.jsの方で記述。

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
+/*
+今回は開発の指示でwebpack+babelを用いてフロント環境を構築してくださいという指示が提示
+されていましたので、webpack+babelの方にvueの環境構築を行いました。
+またsassの環境構築としてgulpを用いて行いました。
+仕上げにpackage.jsonのnpm-scriptでgulpとwebpackを２つ同時並列に読み込む処理を記述して
+vueとsassが同時に使える環境を作りました。
+*/
+
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
 const path = require("path");
-
 const env = process.env.NODE_ENV || "development";
-const glob = require("glob"); //ワイルドカードを使用するため。
+const glob = require("glob"); //ワイルドカード(*)を使用するため。
 
 module.exports = {
     entry: glob.sync("./resources/js/*.js"),
@@ -26,7 +33,7 @@ module.exports = {
     },
     resolve: {
         alias: {
-            vue$: "vue/dist/vue.esm.js",
+            vue$: "vue/dist/vue.esm.js", //←vueのテンプレートを使うために記述
         },
     },
     plugins: [new VueLoaderPlugin()],
