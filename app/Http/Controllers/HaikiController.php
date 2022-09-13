bb986c87a0160f26d74f33d135307f01001546d1
kakutoikemen <karasisuki4902@gmail.com>
2022-09-13 17:35:02 +0900

    haikiのshopper用のファイルと　staff用のファイルを作成　shopper_mypageのみ作成

--- app/Http/Controllers/HaikiController.php
+++ app/Http/Controllers/HaikiController.php
@@ -8,6 +8,6 @@ class HaikiController extends Controller
 {
     //画面を表示するための処理
     public function shopper_mypage_display(){
-        return view('haiki.shopper_mypage_display');
+        return view('haiki_shopper.shopper_mypage_display');
     }
 }
