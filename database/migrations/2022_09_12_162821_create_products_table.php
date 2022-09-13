4b4d043e7cca38f02a12a1ef5cdda5d341648aad
kakutoikemen <karasisuki4902@gmail.com>
2022-09-13 20:58:36 +0900

    prefecture productテーブル　バグは出ない

--- database/migrations/2022_09_12_162821_create_products_table.php
+++ database/migrations/2022_09_12_162821_create_products_table.php
@@ -11,11 +11,26 @@ class CreateProductsTable extends Migration
      *
      * @return void
      */
+
+/**
+ * 商品テーブル
+ * 1 商品のID
+ * 2 商品の名前
+ * 3 商品の画像
+ * 4 商品の日付
+ * 5 購入されているかどうかのチェック(デフォルト０商品購入していない状態)
+ * （1になったら購入されたということにする）
+ * 6 タイムスタンプ
+ */
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->increments('product_id');               //...............1
+            $table->string('product_name','255');           //...............2
+            $table->string('product_pic','255');            //...............3
+            $table->date('best_bydate');                    //...............4
+            $table->boolean('buy_check')->default('0');     //...............5
+            $table->timestamps();                           //...............6
         });
     }
 
@@ -26,6 +41,13 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('products',function (Blueprint $table) {
+            $table->increments('product_id'); 
+            $table->string('product_name','255'); 
+            $table->string('product_pic','255');
+            $table->date('best_bydate');
+            $table->boolean('buy_check')->default('0');
+            $table->timestamps();
+        });
     }
 }
