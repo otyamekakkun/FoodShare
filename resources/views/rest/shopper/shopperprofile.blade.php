d061d525db1ccebbd32a50c4748713fbd8d614da
kakutoikemen <karasisuki4902@gmail.com>
2022-09-21 14:47:32 +0900

    分離成功

--- resources/views/rest/shopper/shopperprofile.blade.php
+++ resources/views/rest/shopper/shopperprofile.blade.php
@@ -4,12 +4,12 @@
 <section>
     <form>
         @csrf
-        <div class="c-formshopperprofile">
-        <h3 class="c-formshopperprofile__title">お客様 プロフィール編集</h3>
-        <div class="c-formshopperprofile__te">
-        <input class="c-formshopperprofile__text" action="post" type="text" name="" placeholder="メールアドレス編集">
-        <input class="c-formshopperprofile__text" action="post" type="password" name="" placeholder="パスワード編集">
-        <input class="c-formshopperprofile__text" action="post" type="password" name="" placeholder="パスワード再入力">
+        <div class="l-formshopperprofile">
+        <h3 class="l-formshopperprofile__title">お客様 プロフィール編集</h3>
+        <div class="l-formshopperprofile__te">
+        <input class="l-formshopperprofile__text" action="post" type="text" name="" placeholder="メールアドレス編集">
+        <input class="l-formshopperprofile__text" action="post" type="password" name="" placeholder="パスワード編集">
+        <input class="l-formshopperprofile__text" action="post" type="password" name="" placeholder="パスワード再入力">
         </div>
     <input type="submit" class="c-formshopperprofile__sub" value="変更する">
     </div>
