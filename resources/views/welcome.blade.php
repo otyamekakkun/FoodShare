8ec0409769c3a2e0dec6cf48f98d04024d0b6196
kakutoikemen <karasisuki4902@gmail.com>
2022-09-21 12:28:39 +0900

    トップ画面作り　制作

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,14 +54,6 @@ haiki shareのトップ画面を作成しする。
 
 
 
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>haiki share TOP画面</title>
-    </head>
-    <body>
         @extends('layouts.app')
 @section('content')
 
