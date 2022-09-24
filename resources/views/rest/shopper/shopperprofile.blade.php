35d3d41c7f1af6d8605986bbec52b7f06dfe53b9
kakutoikemen <karasisuki4902@gmail.com>
2022-09-24 22:44:35 +0900

    エラーメッセージ用のhtmlセッティング完了　email等などのバリデーションチェックの記述はまだ書いていない。

--- resources/views/rest/shopper/shopperprofile.blade.php
+++ resources/views/rest/shopper/shopperprofile.blade.php
@@ -16,23 +16,20 @@
 </span>
 @enderror
 
-        {{-- 
-        <div class="form-group row">
-            <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('Title') }}</label>
-
-            <div class="col-md-6">
-                <input id="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}" autocomplete="title" autofocus>
-
-                @error('title')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-                @enderror
-            </div>
-        </div>
-         --}}
 <input class="l-formshopperprofile__text" action="post" type="password" name="password" placeholder="パスワード編集">
-        <input class="l-formshopperprofile__text" action="post" type="passwordremind" name="password" placeholder="パスワード再入力">
+@error('password')
+<span class="" role="alert">
+    <strong>{{$message}}</strong>
+</span>
+@enderror
+
+        <input class="l-formshopperprofile__text" action="post" type="password" name="password2" placeholder="パスワード再入力">
+        @error('password2')
+<span class="" role="alert">
+    <strong>{{$message}}</strong>
+</span>
+@enderror
+
         </div>
     <input type="submit" class="c-formshopperprofile__sub" value="変更する">
     </div>
--- resources/views/rest/staff/staff_profileform.blade.php
+++ resources/views/rest/staff/staff_profileform.blade.php
@@ -9,11 +9,53 @@
             <h3 class="l-formstaffprofile__title">コンビニ情報編集画面</h3>
             <div class="l-formstaffprofile__te">
             <input class="l-formstaffprofile__text" action="post" type="text" name="email" placeholder="メールアドレス編集">
+{{-- メールアドレス編集のエラーメッセージよう --}}
+            @error('email')
+<span class="" role="alert">
+    <strong>{{$message}}</strong>
+</span>
+@enderror
+
             <input class="l-formstaffprofile__text" action="post" type="password" name="password" placeholder="パスワード編集">
-            <input class="l-formstaffprofile__text" action="post" type="password" name="password" placeholder="パスワード再入力">
+
+            @error('password')
+<span class="" role="alert">
+    <strong>{{$message}}</strong>
+</span>
+@enderror
+
+            <input class="l-formstaffprofile__text" action="post" type="password" name="password2" placeholder="パスワード再入力">
+
+            @error('password2')
+<span class="" role="alert">
+    <strong>{{$message}}</strong>
+</span>
+@enderror
+
             <input class="l-formstaffprofile__text" action="post" type="password" name="convinience_name" placeholder="コンビニ名">
+
+            @error('convinience_name')
+<span class="" role="alert">
+    <strong>{{$message}}</strong>
+</span>
+@enderror
+
             <input class="l-formstaffprofile__text" action="post" type="password" name="convinience_branch" placeholder="支店名">
+
+            @error('convinience_branch')
+<span class="" role="alert">
+    <strong>{{$message}}</strong>
+</span>
+@enderror
+
             <input class="l-formstaffprofile__text" action="post" type="password" name="adress" placeholder="住所">
+
+            @error('adress')
+<span class="" role="alert">
+    <strong>{{$message}}</strong>
+</span>
+@enderror
+
             <input type="submit" class="l-formstaffprofile__sub" value="変更する">
     
         </div>
