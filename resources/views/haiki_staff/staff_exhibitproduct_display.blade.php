3079f99e03a7cbfd2ea1311ffcdf7ebcf1460906
kakutoikemen <karasisuki4902@gmail.com>
2022-09-26 13:59:29 +0900

    ここでの商品登録の状態ok

--- resources/views/haiki_staff/staff_exhibitproduct_display.blade.php
+++ resources/views/haiki_staff/staff_exhibitproduct_display.blade.php
@@ -3,6 +3,7 @@
 {{-- (画面における説明)
     商品を出品する画面
     データベース機能
+    画面モックは今から考え直すが設計図はあるのでそれに準じる
     --}}
 
 <body>
@@ -10,103 +11,62 @@
         <header>
             @include('rest.staff.header')
         </header>
-    
 <h1>
     商品を出品する
 </h1>
 {{-- form画面ここから --}}
 <form method="POST" action="{{ route('create.exhibit') }}" enctype="multipart/form-data">
     @csrf
-
-    <div class="c">
-        出品したい商品を入れてください
-        <div class="form-group row">
-            <label for="img_path" class="col-md-4 col-form-label text-md-right">写真を入れる</label>
+        出品したい商品の画像を入れてください
+            <label for="img_path" class="">写真を入れる</label>
     
-            <div class="col-md-6">
                 <input id="img_path" type="file" class="form-control @error('img_path') is-invalid @enderror" name="img_path" value="{{ old('problem2') }}" autocomplete="problem0" autofocus>
     
                 @error('img_path')
-                <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
-                </span>
                 @enderror
-            </div>
-        </div>
     
     {{--  --}}
 <h1>出品したい商品の名前を入れてください</h1>    
-    <div class="form-group row">
-        <label for="problem2" class="col-md-4 col-form-label text-md-right">商品名</label>
+        <label for="problem2" class="">商品名</label>
 
-        <div class="col-md-6">
-            <input id="product_name" type="text" class="form-control @error('problem2') is-invalid @enderror" name="product_name" value="{{ old('product_name') }}" autocomplete="problem0" autofocus>
+            <input id="" type="text" class="form-control @error('problem2') is-invalid @enderror" name="product_name" value="{{ old('product_name') }}" autocomplete="problem0" autofocus>
 
             @error('product_name')
-            <span class="invalid-feedback" role="alert">
+            <span class="" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
             @enderror
-        </div>
-    </div>
-{{--  --}}
+        <label for="price" class="">金額を入れる</label>
 
-{{--  --}}
-    {{--  --}}
-
-</div>
-<div class="d">
-    <div class="form-group row">
-        <label for="price" class="col-md-4 col-form-label text-md-right">金額を入れる</label>
-
-        <div class="col-md-6">
-            <input id="img_path" type="number" class="form-control @error('price') is-invalid @enderror" name="price" value="{{ old('problem2') }}" autocomplete="problem0" autofocus>
+            <input id="" type="number" class="form-control @error('price') is-invalid @enderror" name="price" value="{{ old('problem2') }}" autocomplete="problem0" autofocus>
 
             @error('price')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
             @enderror
-        </div>
-    </div>
+        <label for="best_by_date" class="">賞味期限を入れる</label>
 
-    価格 円
-
-</div>
-<div class="e">
-    賞味期限 カレンダー形式で入力させる
-
-    <div class="form-group row">
-        <label for="best_by_date" class="col-md-4 col-form-label text-md-right">賞味期限を入れる</label>
-
-        <div class="col-md-6">
-            <input id="best_by_date" type="date" class="form-control @error('best_by_date') is-invalid @enderror" name="best_by_date" value="{{ old('best_by_date') }}" autocomplete="problem0" autofocus>
+            <input id="best_by_date" type="date" class=" @error('best_by_date') is-invalid @enderror" name="best_by_date" value="{{ old('best_by_date') }}" autocomplete="problem0" autofocus>
 
             @error('img_path')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
             @enderror
-        </div>
-    </div>
 
 
-
-</div>
 <button type="submit" class="btn btn-primary">
     商品を出品する
 </button>
 {{-- 全体を括るdiv --}}
-</div>
 
 {{-- 
     formのname属性はレコード名と同じ（マイグレーションと同じにすると楽に処理ができる
     
     
     --}}
-
-                                </div>
-                            </div>
                         </form>
 <div id="app">
 <footer-component></footer-component>
