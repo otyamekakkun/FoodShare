9cb8e9f38da1640be028e435c9f5faca40645c31
kakutoikemen <karasisuki4902@gmail.com>
2022-10-01 15:42:28 +0900

    骨組みは完成したという感じ　まだまだ

--- resources/views/haiki_staff/staff_exhibitproduct_list_display.blade.php
+++ resources/views/haiki_staff/staff_exhibitproduct_list_display.blade.php
@@ -10,29 +10,54 @@
 </h1>
 
 <main>
-    <h1>自分のコンビニが出品した商品</h1>
-    @foreach ($products as $product)
-    <img src="{{ Storage::url($product->img_path) }}" width="25%">
-    <p>商品名:{{$product->product_name}}</p>
-    <p>価格:{{$product->price}}</p>
-    {{-- <button>詳細をみる</button> --}}
-    <p>詳細をみる</p>
-    <button><a href="{{route('haiki_shopper.staff_productdetail_display',$product->id)}}">商品の詳細をみる</a>
+    <div class="l-exhibitproductlist">
+        {{-- <div class="c-productlist"> --}}
 
+{{-- おそらくもっといい方法があると思うが --}}
+
+            <div class="pro">
+                <div class="proa">
+                <div>
+                    <div>
+                        <div>
+    @foreach ($products as $product)
+    <div class="proa">
+        <img src="{{ Storage::url($product->img_path) }}" width="25%">
+        <p>商品名:{{$product->product_name}}</p>
+        <p>価格:{{$product->price}}</p>
+    
+    </div>
+    {{-- <button>詳細をみる</button> --}}
+    <button><a href="{{route('haiki_shopper.staff_productdetail_display',$product->id)}}">商品の詳細をみる</a></button>
     @if($product->bought===0)
     {{-- <button>購入されていないので編集する</button> --}}
     {{-- <a href="{{route('haiki_shopper.staff_productedit_display',$product->product_id)}}">>商品を編集する<</a> --}}
     <button><a href="{{route('haiki_shopper.staff_productedit_display',$product->id)}}">商品を編集する</a>
     </button>
+</div>
 
     @endif
     {{-- おそらく詳細を見る編集するでidをふる練習をしないと行けなさそう。 --}}
 
     @endforeach
+</div>
+
+
+
+
+
+
+
 
     {{$products->links('layouts.paginate')}}
 
-    </main>
+                </div>
+                </div>
+            </div>
+            {{-- 全体のレイアウトを閉じるdiv --}}
+</div>
+</main>
+
 <div id="app">
 <footer-component></footer-component>
 </div>
