3d330f5de27e1de8b3e07592708e15e329cd0a55
kakutoikemen <karasisuki4902@gmail.com>
2022-10-04 11:34:07 +0900

    postパラメータにidwo 降ってもエラーが出ないこの感じ

--- routes/web.php
+++ routes/web.php
@@ -92,7 +92,7 @@ Route::get('/haiki/shopper_mypage',[HaikiController::class,'shopper_mypage_displ
 // shopper_profileのフォーム入力ページ 2
 //================================================================
 Route::get('/haiki/shopper_profile',[HaikiController::class, 'shopper_profile_display'])->name('haiki_shopper.shopper_profile_display');                        //2これは自分専用のページにしたい
-Route::post('/haiki/shopper_profile',[HaikiController::class, 'shopper_profile_edit'])->name('haiki_shopperprofile_edit');
+Route::post('/haiki/{id}/shopper_profile',[HaikiController::class, 'shopper_profile_edit'])->name('haiki_shopperprofile_edit');
 
 //考察１idをパラメータ内に含ませる
 
