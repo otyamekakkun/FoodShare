a797cd476d36a9143c12338b8b3aa59bd67bd041
kakutoikemen <karasisuki4902@gmail.com>
2022-10-18 12:23:02 +0900

    画像の色が変わる

--- resources/js/staff/dragdrop.vue
+++ resources/js/staff/dragdrop.vue
@@ -1,3 +1,51 @@
 <template>
-    <div>こんにリハ</div>
+    <div
+        class="drop_area"
+        　　　　　@dragenter="dragEnter"
+        　　　　　:class="{enter: isEnter}"
+    >
+        ファイルアップロード
+    </div>
 </template>
+<script>
+export default {
+    data: function () {
+        return {
+            isEnter: false,
+        };
+    },
+    methods: {
+        dragEnter() {
+            this.isEnter = true;
+        },
+    },
+};
+</script>
+<style>
+html,
+body {
+    height: 100%;
+}
+
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.drop_area {
+    color: gray;
+    font-weight: bold;
+    font-size: 1.2em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 500px;
+    height: 300px;
+    border: 5px solid gray;
+    border-radius: 15px;
+}
+.enter {
+    border: 10px dotted powderblue;
+}
+</style>
