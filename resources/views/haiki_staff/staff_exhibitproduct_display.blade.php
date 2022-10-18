25dd338c47a403a3732080f958f2448651d2a873
kakutoikemen <karasisuki4902@gmail.com>
2022-10-18 13:45:33 +0900

    topページの画面生成する

--- resources/views/haiki_staff/staff_exhibitproduct_display.blade.php
+++ resources/views/haiki_staff/staff_exhibitproduct_display.blade.php
@@ -9,18 +9,21 @@
 <h1 class="c-title">
     商品を出品する
 </h1>
-<form method="POST" action="{{ route('create.exhibit') }}" enctype="multipart/form-data" class="m">
+<div class="c-staffform">
+    <h1>出品されたい商品の情報について記述してください</h1>
+<form method="POST" action="{{ route('create.exhibit') }}" enctype="multipart/form-data">
     @csrf
+    <div class="c-staffform__area">
         <h1 class="l-productexhibitform__sentence">商品名</h1>
-            <input  type="text" class="l-productexhibitform__typo @error('problem2') is-invalid @enderror" name="product_name" value="" placeholder="商品名">
+            <input  type="text" class= "@error('problem2') is-invalid @enderror" name="product_name" value="" placeholder="商品名">
             @error('product_name')
             <span class="" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
             @enderror
             <br/>
-        <label class="l-productexhibitform__sentence">金額を入れる</label><br>
-            <input id="" type="number" class="l-productexhibitform__typo2 @error('price') is-invalid @enderror" name="price" value="">円
+        <label class="">金額を入れる</label><br>
+            <input id="" type="number" class=" @error('price') is-invalid @enderror" name="price" value="">円
             @error('price')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -35,11 +38,12 @@
         {{-- 登録した都道府県が自動的にデータベースに入るように設定する --}}
 <input type="text" name="prefecture" value="{{$admins->prefecture}}" class="u-hidden">
 {{-- 都道府県エリアここまで --}}
-<button type="submit" class="btn btn-primary">
+<button type="submit" class="">
     商品を出品する
 </button>
-{{-- 全体を括るdiv --}}
+</div>
 </form>
+</div>
 <footer-component></footer-component>
 </div>
 
--- resources/views/rest/other/otherheader.blade.php
+++ resources/views/rest/other/otherheader.blade.php
@@ -1,6 +1,6 @@
 {{-- 
     トップ画面,ログイン画面ユーザー登録画面のヘッダー部分
     --}}
-<div class="l-header">
+<div class="l-toppageheader">
     <h1 class="l-header__title">haiki share</h1>
 </div>
