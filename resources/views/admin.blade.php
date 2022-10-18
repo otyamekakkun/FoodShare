7935117206b16752a472fda9ae56bb54e292ca59
kakutoikemen <karasisuki4902@gmail.com>
2022-10-18 11:44:40 +0900

    場所取りの形はできた

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -6,7 +6,7 @@
     <header>
         @include('rest.staff.header')
     </header>
-<div></div>
+<div class="l-staffmypage">
 <h1>
  {{$admin->convinience_name}}所属
  {{$admin->name}}様haikishare専用ページ
@@ -17,16 +17,10 @@
     <h1>ご購入された商品一覧</h1>
     <a href="{{route('haiki_shopper.staff_buyproduct_display')}}"><button>購入された商品を全権表示</a></button>
 <button><a href="{{route('haiki_shopper.staff_exhibitproduct_list_display')}}"><button>出品した商品一覧を表示</a></button>
-
-
-
-
-
+</div>
 <div id="app">
 {{-- コンポーネント部分 --}}
 <staffmypage-componennt></staffmypage-componennt>
-
-
 <staffmypagebought-componennt></staffmypagebought-componennt>
 <footer-component></footer-component>
 </div>
