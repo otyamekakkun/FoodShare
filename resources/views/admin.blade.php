028c000a6b60f67fcd7e3a5e1a485b4c4df69039
kakutoikemen <karasisuki4902@gmail.com>
2022-10-18 11:54:57 +0900

    もう少し

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -7,16 +7,17 @@
         @include('rest.staff.header')
     </header>
 <div class="l-staffmypage">
-<h1>
+<h1 class="l-staffmypage__title">
  {{$admin->convinience_name}}所属
- {{$admin->name}}様haikishare専用ページ
+ {{$admin->name}}様専用ページ
 </h1>
+<div class="l-staffmypage__menu">
 <p>メニュー</p>
 <a href="{{route('haiki_shopper.staff_profile_display')}}"><button>コンビニプロフィール編集画面</a></button>
 <a href="{{route('haiki_shopper.staff_exhibitproduct_display')}}"><button>商品を出品する</a></button>
-    <h1>ご購入された商品一覧</h1>
     <a href="{{route('haiki_shopper.staff_buyproduct_display')}}"><button>購入された商品を全権表示</a></button>
 <button><a href="{{route('haiki_shopper.staff_exhibitproduct_list_display')}}"><button>出品した商品一覧を表示</a></button>
+</div>
 </div>
 <div id="app">
 {{-- コンポーネント部分 --}}
