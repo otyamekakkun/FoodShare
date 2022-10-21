c62ce8186be52eb894590be67132a01317551977
kakutoikemen <karasisuki4902@gmail.com>
2022-10-21 21:00:35 +0900

    レスポンシブ完了

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,10 +10,8 @@
 <aside>
     <img src="{{asset('img/top.jpg')}}" class="l-topimg">
 </aside>
-<div class="l-top__title"> 
 <h1 class="l-top__title__text">haiki share</h1>
-<h3>環境と安さの相乗効果</h3>
-</div>
+<h3 class=" l-top__title__text2">環境と安さの相乗効果</h3>
 <div id="app">
     <toppage-component ></toppage-component>
     <footer-component></footer-component>
