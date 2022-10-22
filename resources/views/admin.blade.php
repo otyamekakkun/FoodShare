8692546aef8d17385c234e98e6bb4ceb89d422da
kakutoikemen <karasisuki4902@gmail.com>
2022-10-22 16:17:01 +0900

    いらないファイル削除

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -10,18 +10,6 @@
 <h1 class="l-staffmypage__title">
     {{$admin->convinience_name}}所属 {{$admin->name}}様専用メニュー
 </h1>
-    {{-- ここからは廃止
-
-<div class="l-staffmypage__menu">
-<h1 class="l-staffmypage__title"> メニュー</h1>
-<div class="l-staffmypage__buttonarea">
-<button class="c-staff__action__button"><a href="{{route('haiki_shopper.staff_profile_display')}}">コンビニ情報編集</a></button>
-<button class="c-staff__action__button"><a href="{{route('haiki_shopper.staff_exhibitproduct_display')}}">出品する</a></button>
-<button class="c-staff__action__button"> <a href="{{route('haiki_shopper.staff_buyproduct_display')}}">購入された商品リスト</a></button>
-<button class="c-staff__action__button"><a href="{{route('haiki_shopper.staff_exhibitproduct_list_display')}}">出品した商品リスト</a></button>
-</div>
-</div>
- --}}
 </div>
 
 <div id="app">
