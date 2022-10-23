2ddfe91823562f4b660452d79664793977fa3083
kakutoikemen <karasisuki4902@gmail.com>
2022-10-23 13:02:12 +0900

    ボタンのイメージはこれ

--- resources/views/haiki_shopper/shopper_productdetail_display.blade.php
+++ resources/views/haiki_shopper/shopper_productdetail_display.blade.php
@@ -53,34 +53,3 @@
 
 
 
-{{-- 
-    1.写真はvueで読み込む。
-    2.基本的に商品を編集するみたいな容量で記述する。ただしそれよりすごい簡易的で購入フラグと今ログインしているユーザーフラグを
-    更新すれば良い。
-    3.購入後はマイページに遷移する。
-    4既に商品が購入されていたら購入することはできない。ただし購入をキャンセルすることができる。
-    
-    
-    <h1>商品の詳細情報です</h1>
-<div class="l-productdetail">
-    ここで全体図をつける
-<div class="l-productdetail__area">
-<div class="l-productdetail__area__photo">
-
-
-</div>
-
-<div class="l-productdetail__area__menu">
-    ここでメニューバー見たいな感じをつけようかな
-    <button>購入する</button>
-    <button>購入をキャンセルする</button>
-   <button>
-    <a class="btn_tw" href="https://twitter.com/intent/tweet?text=ここを記述するとtwitter内ですでに文が打ち込まれる;" target="_blank_" rel="nofollow"><i class="fab fa-twitter fa-fw"></i> この商品をシェアする</a>
-
-   </button> 
-
-</div>
-</div>
-</div>  
-
-    --}}
