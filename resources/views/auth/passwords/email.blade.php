0fc4115f7a94c00906d5b86da9b64a61f88f11f2
kakutoikemen <karasisuki4902@gmail.com>
2022-10-29 11:27:52 +0900

    イメージ完成

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,13 +1,12 @@
-@extends('layouts.app')
+@extends('layouts.app3')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="c-form1">
+    <div class="c-form__area">
+
 
                 <div class="card-header">{{ isset($authgroup) ? ucwords($authgroup) : ""}} 
                 </div>
-                <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -17,34 +16,30 @@
 
                     @isset($authgroup)
                     {{-- パスワードリマインダー --}}
+                    {{-- <div class="c-form1"> --}}
+                        {{-- <div class="c-form__area"> --}}
+
                     コンビニ用パスワードリマインダー
                     <form method="POST" action="{{ route("$authgroup.password.email") }}">
-
                     @else
                     <form method="POST" action="{{ route('password.email') }}">
                     @endisset
                         @csrf
+                            <span for="email" class="c-form__area__message">{{ __('Email Address') }}:</span>
+                            @error('email')
+                            <span class="c-errormessage" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                        <br/>
 
-                        <div class="row mb-3">
-                            <span for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</span>
+                                <input id="email" type="email" class="c-form__area__text" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
 
                                 <button type="submit" class="btn btn-primary">
                                     新たなパスワードを取得する
                                 </button>
                     </form>
                 </div>
-        </div>
-    </div>
 </div>
 @endsection
