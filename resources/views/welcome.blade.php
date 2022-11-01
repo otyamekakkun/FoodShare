83e04968924edfef18f899e0c98379c0483a0df9
kakutoikemen <karasisuki4902@gmail.com>
2022-11-01 14:58:43 +0900

    まだ

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,12 +8,11 @@
 </header>
 <body>
     <div class="l-topimg">
-
 {{-- <img src="{{asset('img/top.jpg')}}" class="l-topimg"> --}}
 {{-- 背景をかえる --}}
 <div class="l-top__titlearea">
 <h1 class="l-top__title__text">haiki share</h1>
-<h3 class=" l-top__title__text2">convenience store</h3>
+<h3 class="l-top__title__text2">convenience store</h3>
 </div>
 
 {{-- ボタンエリアを作る --}}
