303ddd683c9f76e17ce553a693f60660c4939774
kakutoikemen <karasisuki4902@gmail.com>
2022-11-02 21:23:29 +0900

    出品された商品画面のマイページ表示補正終了

---
+++ resources/js/staffmypage/component/mypageproduct.vue
@@ -0,0 +1,54 @@
+<template>
+    <div id="app">
+        <main>
+            <div class="c-staffmypagelistarea">
+                <h1 class="c-mypagemenu__title">これまでに出品された商品</h1>
+                <ul class="c-productlist__items" v-for="item in getItems">
+                    <div v-if="item.bought >= 1">
+                        <img
+                            v-bind:src="item.img_path"
+                            class="c-productlist__items__img"
+                        />
+                        <p class="c-productlist__items__img__title">
+                            購入済み！
+                        </p>
+                    </div>
+                    <div v-if="item.bought <= 0">
+                        <img
+                            v-bind:src="item.img_path"
+                            class="c-productlist__items__img2"
+                        />
+                    </div>
+                    <div class="c-productlist__items__list">
+                        <li>商品名.{{ item.product_name }}</li>
+                        <li>お値段.{{ item.price }}円.</li>
+                        <div class="c-productlist__items__bottom">
+                            <button class="c-productlist__items__bottom__area">
+                                <a
+                                    v-bind:href="`haiki/${item.id}}/staff_productdetail`"
+                                    >詳細にいく</a
+                                >
+                            </button>
+                            <div v-if="item.bought <= 0">
+                                <button
+                                    class="c-productlist__items__bottom__area"
+                                >
+                                    <a
+                                        v-bind:href="`haiki/${item.id}}/staff_productedit`"
+                                        >商品を編集する</a
+                                    >
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </ul>
+            </div>
+        </main>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["products", "getItems"],
+};
+</script>
