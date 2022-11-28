31aa925f97ba042726760db7cd77d64ae6a8ffa3
kakutoikemen <karasisuki4902@gmail.com>
2022-11-28 15:07:04 +0900

    修正しました

--- public/js/app.js
+++ public/js/app.js
@@ -6493,10 +6493,10 @@ var render = function render() {
     }
   }, [_c("main", [_vm._m(0), _vm._v(" "), _c("div", {
     staticClass: "c-staffmypagelistarea"
-  }, [_c("div", {
-    staticClass: "c-staffmypagelistarea__area"
   }, _vm._l(_vm.getItems, function (item) {
-    return _c("div", [item.bought >= 1 ? _c("div", [_c("div", {
+    return item.bought >= 1 ? _c("div", {
+      staticClass: "c-staffmypagelistarea__area"
+    }, [_c("div", [_c("div", {
       staticClass: "c-staffmypagelistarea__list"
     }, [_c("img", {
       staticClass: "c-staffmypagelistarea__list__img",
@@ -6505,21 +6505,19 @@ var render = function render() {
       }
     }), _vm._v(" "), _c("p", {
       staticClass: "c-staffmypagelistarea__list__bought"
-    }, [_vm._v("\n                                Sold Out\n                            ")]), _vm._v(" "), _c("div", {
+    }, [_vm._v("\n                            Sold Out\n                        ")]), _vm._v(" "), _c("div", {
       staticClass: "c-staffmypagelistarea__text"
     }, [_c("li", {
       staticClass: "c-staffmypagelistarea__list__name"
-    }, [_vm._v("\n                                    " + _vm._s(item.product_name) + "\n                                ")]), _vm._v(" "), _c("li", {
+    }, [_vm._v("\n                                " + _vm._s(item.product_name) + "\n                            ")]), _vm._v(" "), _c("li", {
       staticClass: "c-staffmypagelistarea__list__price"
-    }, [_vm._v("\n                                    " + _vm._s(item.price) + "円.\n                                ")]), _vm._v(" "), _c("a", {
+    }, [_vm._v("\n                                " + _vm._s(item.price) + "円.\n                            ")]), _vm._v(" "), _c("a", {
       staticClass: "c-staffmypagelistarea__list__a",
       attrs: {
         href: "haiki/".concat(item.id, "}/staff_productdetail")
       }
-    }, [_vm._v("詳細を見る↗︎")])])])]) : _vm._e(), _vm._v(" "), item.bought <= 0 ? _c("div", {
-      staticClass: "u-hidden"
-    }) : _vm._e()]);
-  }), 0)])])]);
+    }, [_vm._v("詳細を見る↗︎")])])])])]) : _vm._e();
+  }), 0)])]);
 };
 
 var staticRenderFns = [function () {
--- resources/js/staffmypage/component/boughtmypage.vue
+++ resources/js/staffmypage/component/boughtmypage.vue
@@ -10,37 +10,37 @@
             <!-- <h1 class="c-mypagemenu__title">これまでに購入された商品</h1> -->
             <!-- 購入されたら表示される仕組みになる -->
             <div class="c-staffmypagelistarea">
-                <div class="c-staffmypagelistarea__area">
-                    <div v-for="item in getItems">
-                        <div v-if="item.bought >= 1">
-                            <div class="c-staffmypagelistarea__list">
-                                <img
-                                    v-bind:src="item.img_path"
-                                    class="c-staffmypagelistarea__list__img"
-                                />
-                                <p class="c-staffmypagelistarea__list__bought">
-                                    Sold Out
-                                </p>
-                                <div class="c-staffmypagelistarea__text">
-                                    <li
-                                        class="c-staffmypagelistarea__list__name"
-                                    >
-                                        {{ item.product_name }}
-                                    </li>
-                                    <li
-                                        class="c-staffmypagelistarea__list__price"
-                                    >
-                                        {{ item.price }}円.
-                                    </li>
-                                    <a
-                                        v-bind:href="`haiki/${item.id}}/staff_productdetail`"
-                                        class="c-staffmypagelistarea__list__a"
-                                        >詳細を見る↗︎</a
-                                    >
-                                </div>
+                <div
+                    v-for="item in getItems"
+                    class="c-staffmypagelistarea__area"
+                    v-if="item.bought >= 1"
+                >
+                    <div>
+                        <!-- <div v-if="item.bought >= 1"> -->
+                        <div class="c-staffmypagelistarea__list">
+                            <img
+                                v-bind:src="item.img_path"
+                                class="c-staffmypagelistarea__list__img"
+                            />
+                            <p class="c-staffmypagelistarea__list__bought">
+                                Sold Out
+                            </p>
+                            <div class="c-staffmypagelistarea__text">
+                                <li class="c-staffmypagelistarea__list__name">
+                                    {{ item.product_name }}
+                                </li>
+                                <li class="c-staffmypagelistarea__list__price">
+                                    {{ item.price }}円.
+                                </li>
+                                <a
+                                    v-bind:href="`haiki/${item.id}}/staff_productdetail`"
+                                    class="c-staffmypagelistarea__list__a"
+                                    >詳細を見る↗︎</a
+                                >
                             </div>
                         </div>
-                        <div v-if="item.bought <= 0" class="u-hidden"></div>
+                        <!-- </div> -->
+                        <!-- <div v-if="item.bought <= 0" class="u-hidden"></div> -->
                     </div>
                 </div>
             </div>
