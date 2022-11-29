f665225b1d497cea95c4cd753d4912868118b289
kakutoikemen <karasisuki4902@gmail.com>
2022-11-29 13:44:47 +0900

    トップ画面変更　そのもと作り

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,20 +2,26 @@
 @extends('layouts.app3')
 @section('content')
 <body>
+    {{--
 <header>
     @include('header.other.otherheader')
 </header>
+  --}}
 <body>
+    <div class="l-top">
     <div class="l-topimg">
+    </div>
 <div class="l-top__titlearea">
-<h1 class="l-top__title__text">haiki share</h1>
-<h3 class="l-top__title__text2">convenience store</h3>
+<h1 class="l-top__title__tex">haiki share</h1>
+<h3 class="l-top__title__tex">convenience store</h3>
 </div>
 {{-- ボタンエリアを作る --}}
 <div id="app">
     <toppage-component></toppage-component>
 </div>
 </div>
+</div>
+
 </body>
 
 </html>
