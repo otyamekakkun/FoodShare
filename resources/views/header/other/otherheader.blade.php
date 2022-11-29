19d9dc58795e7f46cf932d0d5b8881dc24947254
kakutoikemen <karasisuki4902@gmail.com>
2022-11-30 01:16:50 +0900

    rf

--- resources/views/header/other/otherheader.blade.php
+++ resources/views/header/other/otherheader.blade.php
@@ -2,5 +2,5 @@
     トップ画面,ログイン画面ユーザー登録画面のヘッダー部分
     --}}
 <div class="l-toppageheader">
-    <h1 class="l-header__title">haiki share</h1>
+    <h1 class="l-header__toptitle">HAIKI SHARE</h1>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,38 +1,55 @@
 {{--トップ画面を作成する画面 --}}
 @extends('layouts.app3')
 @section('content')
-<body>
-    {{--
 <header>
     @include('header.other.otherheader')
 </header>
-  --}}
+{{-- l-top__title__text --}}
 <body>
-    <div class="l-top">
-    <div class="l-topimg"></div>
-<div class="l-top__titlearea">
-<h1 class="l-top__title__text">haiki share</h1>
-<h3 class="l-top__title__tex">convenience store</h3>
-    <div>
-        <a href="login">
-            お客様ログイン
-        </a>
-        <br />
-        <a href="./register">
-            お客様ユーザー登録
-        </a>
-        <br />
-        <a href="./login/admin">
-            コンビニログイン
-        </a>
-        <br />
-        <a href="./register/admin">
-            コンビニユーザー登録
-        </a>
-        <br />
 
-</div>
-</div>
+ {{-- <div class="l-top"> --}}
+    <div class="l-topimg">
+    <img src="https://s3.ap-northeast-1.amazonaws.com/webukatu-haikikaku.com/awss3/toppage.jpg">
+    <a href="login">
+        お客様ログイン
+    </a>
+    <br />
+    <a href="./register">
+        お客様ユーザー登録
+    </a>
+    <br />
+    <a href="./login/admin">
+        コンビニログイン
+    </a>
+    <br />
+    <a href="./register/admin">
+        コンビニユーザー登録
+    </a>
+    <br />
+
+
+{{-- <h1 class="">haiki share</h1> --}}
+{{-- <h3 class="">convenience store</h3> 
+    <a href="login">
+        お客様ログイン
+    </a>
+    <br />
+    <a href="./register">
+        お客様ユーザー登録
+    </a>
+    <br />
+    <a href="./login/admin">
+        コンビニログイン
+    </a>
+    <br />
+    <a href="./register/admin">
+        コンビニユーザー登録
+    </a>
+    <br />
+--}}
+
+    </div>
+    </div>
 </body>
 
 </html>
