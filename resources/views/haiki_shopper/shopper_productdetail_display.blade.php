be4a01920e45970662ecb3cb089a6074e88dc1d1
kakutoikemen <karasisuki4902@gmail.com>
2022-12-02 12:30:43 +0900

    全ての過程を終了した　ここからコードをきれいにする

--- resources/views/haiki_shopper/shopper_productdetail_display.blade.php
+++ resources/views/haiki_shopper/shopper_productdetail_display.blade.php
@@ -1,42 +1,31 @@
 {{-- 
     商品詳細画面のお客様専用で表したもの
-    （概要)
-    (データベースのproductsテーブルのboughtが０の時。)
-    データベースのproductsテーブルのboughtを見て0なら商品が購入されていないので、画面上で商品を買う！というスイッチが表示される。
-    商品を買うというボタンが押されたら、ログインしているユーザーと出品したコンビニにメールがいくようにされるのとboughtが１となり、
-    購入済みというラベルが全画面に表示される。
-
-   (データベースのproductsテーブルのboughtが１の時)
-   データベースのproductsテーブルのboughtを見て０なら商品が購入されているので、画面上で商品の購入をキャンセルするという処理が表示される。
-   このボタンが押された際に、boughtの値が0となり、購入される前と同じ状態になる。
-
-   (twitterの実装について)
-   今回は「この商品をシェアするというボタンが押されたら、twitter内で商品情報（名前）が記述されるようにした
-
-   (productdetail-component)
-   商品の画像、名前、賞味期限、金額、出品都道府県、購入フラグをvueのproductdetail-componentに渡して商品の詳細を表示したもの。
-   コンビニ側の商品詳細と併用している。
-    --}}
+--}}
 @extends('layouts.app2')
 @section('content')
 @section('title', "商品詳細画面")
 <body>
-        <header>
-            @include('header.shopper.shoppermypage_header') 
-            </header>
-            <h1 class="c-detail__title">商品詳細</h1>
-
+    <header>
+        @include('header.shopper.shoppermypage_header') 
+    </header>
+<h1 class="c-detail__title">商品詳細</h1>
 <div id="app">
     <main>
          <div> 
-        <productdetail-component :id="'{{$products->id}}'" :img="'{{$products->img_path}}'" :name="'{{$products->product_name}}'" :price="'{{$products->price}}'"
-            :best_by_date="'{{$products->best_by_date}}'" :prefecture="'{{$products->prefecture}}'" :bought="'{{$products->bought}}'" :convinience_name="'{{$products->convinience_name}}'"
-            :updated_at="'{{$products->updated_at}}'"
+    <productdetail-component
+        :id="'{{$products->id}}'" 
+        :img="'{{$products->img_path}}'" 
+        :name="'{{$products->product_name}}'" 
+        :price="'{{$products->price}}'"
+        :best_by_date="'{{$products->best_by_date}}'" 
+        :prefecture="'{{$products->prefecture}}'"
+        :bought="'{{$products->bought}}'"
+        :convinience_name="'{{$products->convinience_name}}'"
+        :updated_at="'{{$products->updated_at}}'"
             >
-        </productdetail-component>
+    </productdetail-component>
 @if($products->bought<=0)
-    <form class="" action="{{ route('bought.update',$products->id)}}" enctype="multipart/form-data" method="post">
-                
+    <form class="" action="{{ route('bought.update',$products->id)}}" enctype="multipart/form-data" method="post">   
             <a href="https://twitter.com/intent/tweet?text=haiki share 商品名{{$products->product_name}}について自由に書いて広めよう!!" target="_blank_" rel="nofollow"
                 class="c-productdetail__twitter" 
                 >
@@ -51,7 +40,6 @@
     <a  href="https://twitter.com/intent/tweet?text=haiki share 商品名{{$products->product_name}}について自由に書いて広めよう!!" target="_blank_" rel="nofollow"
         class="c-productdetail__twitter" >
         Twitterでシェアする</a>
-    {{-- </button>  --}}
     <form class=""  action="{{route('bought.cancel',$products->id)}}" enctype="multipart/form-data" method="post">
         @csrf 
             <button class="c-productdetail__button">購入をやめる</button>
@@ -60,8 +48,6 @@
 @endif
 </main>
 <footer-component></footer-component>
-
-{{-- </div> --}}
 </div>
 
 </body>
--- resources/views/haiki_staff/staff_productdetail_display.blade.php
+++ resources/views/haiki_staff/staff_productdetail_display.blade.php
@@ -4,11 +4,8 @@
 @extends('layouts.app2') @section('content') @section('title', "商品詳細画面")
 <body>
     <header>@include('header.staff.staffmypage_header')</header>
-
+    <h1 class="c-detail__title">商品詳細</h1>
     <div id="app">
-        <h1 class="c-detail__title">商品詳細</h1>
-
-        <div class="l-productdetail">
             <productdetail-component
                 :id="'{{$products->id}}'"
                 :img="'{{$products->img_path}}'"
@@ -20,8 +17,13 @@
                 :convinience_name="'{{$products->convinience_name}}'"
                 :updated_at="'{{$products->updated_at}}'"
             ></productdetail-component>
-        </div>
-        <footer-component></footer-component>
-    </div>
+            <a href="https://twitter.com/intent/tweet?text=haiki share 商品名{{$products->product_name}}について自由に書いて広めよう!!" target="_blank_" rel="nofollow"
+                class="c-productdetail__twitter" 
+                >
+                Twitterでシェアする
+            </a>
+            <div class="u-productdetail"></div>
+    <footer-component></footer-component>
+</div>
 </body>
 @endsection
