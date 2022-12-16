86bd9a2c6384b62932365d44936e1ab4b9a9f71b
otyamekakkun <ankengahosii7928@gmail.com>
2022-12-16 13:25:32 +0900

    データベース更新

--- app/Mail/ContactReply.php
+++ app/Mail/ContactReply.php
@@ -2,11 +2,8 @@
 // メール機能の実装。買われる商品名は毎回違うので動的に表現した。
 namespace App\Mail;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use App\Http\Controllers\HaikiController;
-use App\Http\Controllers\shopper\ProductController;
 class Test extends Mailable
 {
     use Queueable, SerializesModels;
